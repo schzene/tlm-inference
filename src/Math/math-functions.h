8c91049b9341319daa5b0f1f0869e2b009fe7a0a
Chenytstu <chenyt_x@163.com>
2023-12-14 16:20:48 +0800

    change Third-party version

---
+++ src/Math/math-functions.h
@@ -0,0 +1,13 @@
+#include <cassert>
+#include <cstring>
+#include <vector>
+
+#include "BuildingBlocks/aux-protocols.h"
+#include "BuildingBlocks/truncation.h"
+#include "BuildingBlocks/value-extension.h"
+#include "LinearOT/linear-ot.h"
+#include "Millionaire/millionaire.h"
+#include "Millionaire/equality.h"
+#include "utils/constants.h"
+#include "utils/utils.h"
+#include "utils/emp-tool.h"
