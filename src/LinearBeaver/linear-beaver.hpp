d226a6e1dd348fcd4f994e8b9b125391c12e5bbf
schzene <schzene@outlook.com>
2023-12-18 16:37:41 +0800

    reduced communication

--- src/LinearBeaver/linear-beaver.hpp
+++ src/LinearBeaver/linear-beaver.hpp
@@ -78,49 +78,6 @@ public:
 
     ~LinearBeaver(){};
 
-    uint64_t *vector_multiplication(uint64_t *x, uint64_t *y, size_t size,
-                                    uint64_t a, uint64_t b, uint64_t c) {
-        uint64_t *s = new uint64_t[size];
-        uint64_t *t = new uint64_t[size];
-        size_t i;
-        for (i = 0; i < size; i++) {
-            s[i] = x[i];
-            t[i] = y[i];
-        }
-        sub(s, size, a);
-        sub(t, size, b);
-        iopack->io->send_data(s, sizeof(uint64_t) * size);
-        iopack->io->send_data(t, sizeof(uint64_t) * size);
-
-        uint64_t *s1 = new uint64_t[size];
-        uint64_t *t1 = new uint64_t[size];
-        iopack->io->recv_data(s1, sizeof(uint64_t) * size);
-        iopack->io->recv_data(t1, sizeof(uint64_t) * size);
-
-        for (i = 0; i < size; i++) {
-            s[i] += s1[i];
-            t[i] += t1[i];
-            s1[i] = s[i];
-            t1[i] = t[i];
-        }
-        mul(t, size, a);             // at
-        mul(s, size, b);             // bs
-        auto st = mul(s1, t1, size); // st
-        for (i = 0; i < size; i++) {
-            s[i] = s[i] + t[i] + c; // at + bs + c;
-        }
-        delete[] t;
-        delete[] s1;
-        delete[] t1;
-        if (party == sci::ALICE) {
-            for (i = 0; i < size; i++) {
-                s[i] = s[i] + st[i]; // at + bs + c + st;
-            }
-        }
-        delete[] st;
-        return s;
-    };
-
     uint64_t *matrix_multiplication(uint64_t *X, uint64_t *Y,
                                           size_t dim1, size_t dim2, size_t dim3,
                                           uint64_t a, uint64_t b, uint64_t c) {
